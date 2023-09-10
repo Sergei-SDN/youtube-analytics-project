05f7d66c627d9a1c34b989ffad880f8fd325561c
Sergei <you@example.com>
2023-09-10 22:36:48 +0800

    homework-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,25 @@
 
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv('YT_API_KEY')
+
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel_data, indent=4, ensure_ascii=False))
